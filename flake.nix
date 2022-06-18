258dd7f281234189f2dc36264c9e48cf44d4dd0e
Martin Toman <toman.martin@live.com>
2022-06-18 12:53:04 -0700

    Use MODULUSREBUS/nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    rust-overlay.url = github:oxalica/rust-overlay/master;
-    naersk.url = github:nmattia/naersk/master;
+    nix.url = github:MODULUSREBUS/nix/master;
   };
 
-  outputs = { self, nixpkgs, rust-overlay, naersk }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-
-      # Function to generate a set based on supported systems
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        overlays = [
-          rust-overlay.overlay
-        ];
-      });
-
-    in {
-      devShell = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-          rust = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [
-              "rust-src"
-              "rls-preview"
-            ];
-            targets = [
-              "x86_64-unknown-linux-gnu"
-            ];
-          };
-        in import ./shell.nix {
-          inherit pkgs rust;
-        });
-    };
+  outputs = { self, nix }:
+    with nix.lib;
+    eachSystem [ system.x86_64-linux ] (system: {
+      devShell = import ./shell.nix {
+        pkgs = nix.packages.${system};
+      };
+  });
 }
--- shell.nix
+++ shell.nix
@@ -1,7 +1,19 @@
-{ pkgs, rust }:
+{ pkgs }:
 
 with pkgs;
 
+let
+  rust = pkgs.rust-bin.stable.latest.default.override {
+    extensions = [
+      "rust-src"
+      "rls-preview"
+    ];
+    targets = [
+      "x86_64-unknown-linux-gnu"
+    ];
+  };
+
+in
 mkShell {
   buildInputs = [
     git
