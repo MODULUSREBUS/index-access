e8bfaf0826c80438bf8325f932a04edad68ffe09
Martin Toman <toman.martin@live.com>
2022-12-06 19:46:49 -0800

    Nix flake update + direnv-nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,29 @@
 {
   inputs = {
-    nix.url = github:MODULUSREBUS/nix/master;
+    nix.url = github:MODULUSREBUS/nix;
   };
 
   outputs = { self, nix }:
     with nix.lib;
     eachSystem [ system.x86_64-linux ] (system: {
-      devShell = import ./shell.nix {
+      devShell = let
         pkgs = nix.packages.${system};
+        custom-rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+          ];
+          targets = [
+            "x86_64-unknown-linux-gnu"
+          ];
+        };
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+          hub
+
+          custom-rust
+          cargo-edit
+        ];
       };
   });
 }
